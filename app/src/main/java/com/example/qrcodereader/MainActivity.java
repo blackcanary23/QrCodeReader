a0fe1b3fe85ef39eea80bf0b9301b09167f889d6
Iris West <ikrasovitova23@gmail.com>
2020-09-25 17:54:47 +0300

    First Part - Qr Reader

--- app/src/main/java/com/example/qrcodereader/MainActivity.java
+++ app/src/main/java/com/example/qrcodereader/MainActivity.java
@@ -1,36 +1,13 @@
 package com.example.qrcodereader;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import android.Manifest;
-import android.content.pm.PackageManager;
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.SparseArray;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements QrScanFragment.OnFragmentInteractionListener {
 
-    SurfaceView cameraView;
-    CameraSource cameraSource;
-    BarcodeDetector barcodeDetector;
-    TextView barcodeInfo;
-    Button readButton;
-    ArrayList<String> links = new ArrayList<>();
+    private FragmentManager fMan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,99 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cameraView = findViewById(R.id.camera_view);
-        barcodeInfo = findViewById(R.id.txtContent);
-        readButton = findViewById(R.id.read);
+        fMan = getSupportFragmentManager();
 
-        barcodeDetector =
-                new BarcodeDetector.Builder(this)
-                        .setBarcodeFormats(Barcode.QR_CODE)
-                        .build();
+        if (savedInstanceState == null) {
 
-        cameraSource = new CameraSource
-                .Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(300, 300)
-                .build();
-
-        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
-
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-
-                try {
-
-                    if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                            Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.CAMERA},1);
-                    }
-                    cameraSource.start(cameraView.getHolder());
-                }
-                catch (IOException ie) {
-
-                    Log.e("CAMERA SOURCE", Objects.requireNonNull(ie.getMessage()));
-                }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-
-                cameraSource.stop();
-            }
-        });
-    }
-
-
-    public void onReadButtonClick(View view) {
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-
-                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-
-                if (barcodes.size() != 0) {
-                    barcodeInfo.post(new Runnable() {
-
-                        public void run() {
-
-                            parseRawData(barcodes.valueAt(0).rawValue);
-                            barcodeInfo.setText(links.get(0));
-                        }
-                    });
-                }
-            }
-        });
-    }
-
-    private void parseRawData(String text) {
-
-        String regex = "\\(?\\b(https://|www[.]|http://)[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]";
-
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(text);
-
-        while(m.find()) {
-
-            String urlStr = m.group();
-
-            if (urlStr.startsWith("(") && urlStr.endsWith(")")) {
-
-                urlStr = urlStr.substring(1, urlStr.length() - 1);
-            }
-            links.add(urlStr);
+            QrScanFragment swFrag = new QrScanFragment();
+            fMan.beginTransaction()
+                    .add(R.id.container, swFrag, "qsFrag")
+                    .commitAllowingStateLoss();
         }
+    }
+
+
+    @Override
+    public void onFragmentInteraction(String rawData) {
+
+        UrlListFragment ulFrag = new UrlListFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("rawData", rawData);
+        ulFrag.setArguments(bundle);
+
+        fMan.beginTransaction()
+                .replace(R.id.container, ulFrag, "ulFrag")
+                .addToBackStack(null)
+                .commitAllowingStateLoss();
     }
 }
